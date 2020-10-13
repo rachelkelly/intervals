07d8cea4fb797a5e0fb4ac0391cc85924bde7f0e
Rachel Kelly <subs@rkode.com>
2020-10-13 15:27:22 -0700

    OK IT RUNS with lots of TBs

---
+++ runintervals/src/runintervals/__init__.py
@@ -0,0 +1,9 @@
+def __init__(self, runsNumber=5, runsLength=5, breakLength=1):
+    """
+    Instantiate runsNumber, runsLength, & breakLength, so these can be
+    taken from the user.
+    These values are the only vars in scope throughout the class
+    """
+    self.runsNumber = runsNumber
+    self.runsLength = runsLength
+    self.breakLength = breakLength
--- runintervals/src/runintervals/app.py
+++ runintervals/src/runintervals/app.py
@@ -9,16 +9,9 @@ from toga.style.pack import COLUMN, ROW
 
 
 class RunIntervals(toga.App):
-
-    def __init__(self, runsNumber=5, runsLength=5, breakLength=1):
-        """
-        Instantiate runsNumber, runsLength, & breakLength, so these can be
-        taken from the user.
-        These values are the only vars in scope throughout the class
-        """
-        self.runsNumber = runsNumber
-        self.runsLength = runsLength
-        self.breakLength = breakLength
+    """
+    __init__ values in __init__.py
+    """
 
     def startup(self):
         """
